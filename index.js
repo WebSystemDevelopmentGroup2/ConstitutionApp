a5ca32453c244e537c11f826b31f578547c0caf2
woods-11 <woodsc5@rpi.edu>
2021-09-22 16:30:57 -0400

    Non working version of the pop up

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// When the user clicks on div, open the popup
+function popUpFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
