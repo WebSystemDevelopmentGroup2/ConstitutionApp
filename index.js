f438e933e8fa0c70d77721b839e6034fbae90e8a
Chris Reed <cjreed121@users.noreply.github.com>
2021-09-22 23:37:11 -0400

    Add pop ups

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+$(document).ready(() => {
+  $(".info-button").click(function() {
+    var content = $(this).parent().parent().next();
+    
+    $("#popup-content").html(content.html());
+    $("#popup-title").text(content.attr("popup-title"));
+    $("#popup-window").show();
+  });
+
+  $("#close-popup").click(function() {
+    $(this).parent().parent().hide();
+  })
+});
+
